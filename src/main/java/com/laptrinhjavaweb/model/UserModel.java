746f32a703890fb9db19934574a53c8c9d24bf38
DuyDung <dinhvandung791@gmail.com>
2020-08-13 12:34:51 +0700

    design model

---
+++ src/main/java/com/laptrinhjavaweb/model/UserModel.java
@@ -0,0 +1,71 @@
+package com.laptrinhjavaweb.model;
+
+public class UserModel extends AbstractModel<UserModel> {
+
+	private String userName;
+	private String fullName;
+	private String password;
+	private String birthDay;
+	private String email;
+	private int phone;
+	private String address;
+	private Long roleId;
+	private int status;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getBirthDay() {
+		return birthDay;
+	}
+	public void setBirthDay(String birthDay) {
+		this.birthDay = birthDay;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Long getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+}
