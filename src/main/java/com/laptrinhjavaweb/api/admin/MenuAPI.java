6034538c750adcc9d940d856fd1732ff2dc288a3
DuyDung <dinhvandung791@gmail.com>
2020-08-20 00:29:20 +0700

    update delete menu

--- src/main/java/com/laptrinhjavaweb/api/admin/MenuAPI.java
+++ src/main/java/com/laptrinhjavaweb/api/admin/MenuAPI.java
@@ -2,6 +2,8 @@ package com.laptrinhjavaweb.api.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +19,11 @@ public class MenuAPI {
 	
 	@Autowired
 	IMenuDadService menuDadService;
+	
+	@GetMapping("/api/menu/{id}")
+	public MenuDadModel findAllMenu(@RequestBody @PathVariable("id") long id) {
+		return menuDadService.findOne(id);
+	}
 	
 	@PostMapping("/api/menu")
 	public MenuDadModel createNew(@RequestBody MenuDadModel menuDadModel) {
--- src/main/java/com/laptrinhjavaweb/model/MenuDadModel.java
+++ src/main/java/com/laptrinhjavaweb/model/MenuDadModel.java
@@ -1,16 +1,10 @@
 package com.laptrinhjavaweb.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.laptrinhjavaweb.entity.MenuChildEntity;
-
 public class MenuDadModel extends AbstractModel<MenuDadModel> {
 	
 	private String name;
 	private String code;
 	
-	private List<MenuChildEntity> menuChilds = new ArrayList<>();
 	
 	public String getName() {
 		return name;
@@ -27,14 +21,5 @@ public class MenuDadModel extends AbstractModel<MenuDadModel> {
 	public void setCode(String code) {
 		this.code = code;
 	}
-
-	public List<MenuChildEntity> getMenuChilds() {
-		return menuChilds;
-	}
-
-	public void setMenuChilds(List<MenuChildEntity> menuChilds) {
-		this.menuChilds = menuChilds;
-	}
-	
 	
 }
